698777713e5a154ce91712a9e449cc6a2240c4a2
xshiba <92438125+xshiba@users.noreply.github.com>
2022-11-26 11:08:26 +0700

    Update and rename README.md to Load.lua

---
+++ Load.lua
@@ -0,0 +1,22 @@
+task.spawn(function()
+    local gg = getrawmetatable(game)
+    local old = gg.__namecall
+    setreadonly(gg,false)
+    gg.__namecall = newcclosure(function(...)
+        local method = getnamecallmethod()
+        local args = {...}
+        if tostring(method) == "InvokeServer" then
+            if tostring(args[1]) == "SkillAction" then
+                if (args[3]) then
+                    if args[3]["MouseHit"] and (_G.Settings.AutoFarmLevel or _G.Settings.AutoSeaKing or _G.Settings.AutoHydra or _G.Settings.AutoGhostShip) and MobP then
+                        pcall(function()
+                            args[3]["MouseHit"] = MobP
+                            return old(unpack(args))
+                        end)
+                    end
+                end
+            end
+        end
+        return old(...)
+    end)
+end)
